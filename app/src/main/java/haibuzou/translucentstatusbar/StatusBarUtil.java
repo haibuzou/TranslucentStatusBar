2c41918f8100d3ebccdfc0d1554c7032ff8730dc
haibuzou <542464069@qq.com>
2016-09-02 20:16:47 +0800

    init Commit

--- app/src/main/java/haibuzou/translucentstatusbar/StatusBarUtil.java
+++ app/src/main/java/haibuzou/translucentstatusbar/StatusBarUtil.java
@@ -3,16 +3,17 @@ package haibuzou.translucentstatusbar;
 
 import android.app.Activity;
 import android.os.Build;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 
-public class StatusBarAdapt {
+public class StatusBarUtil {
 
     public static void setStatusBarColor(Activity activity,int color){
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.KITKAT&&Build.VERSION.SDK_INT<Build.VERSION_CODES.LOLLIPOP) {
             activity.getWindow().setFlags(
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            activity.getWindow().getDecorView();
+            ViewGroup contentView = (ViewGroup)activity.getWindow().getDecorView().findViewById(android.R.id.content);
 
 
         }else if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
